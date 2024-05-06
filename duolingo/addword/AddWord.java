48f163e1eaa805cdc7750a963edbcc08c51ab36b
Marco Gil Flores <159430116+ElMafiosoSalesiano@users.noreply.github.com>
2024-05-06 12:07:40 +0100

    build(AddWord): Now can start to use
    Add word static method is finish now. And we can implement this with other functions.

--- duolingo/addword/AddWord.java
+++ duolingo/addword/AddWord.java
@@ -1,5 +1,18 @@
 package duolingo.addword;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class AddWord {
-    
+    private static Set<String> wordAdded = new HashSet<>();
+
+    private static void wordAdd(String word){
+        word.toLowerCase();
+        if (word.isEmpty()) {
+            System.out.println("No introdujo nada");
+        }
+        else{
+            wordAdded.add(word);
+        }
+    }
 }
